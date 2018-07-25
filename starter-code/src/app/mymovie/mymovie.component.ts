1607780c43faca686fd1282b814808f5c88963f9
Verónica Reina <veroreinah@gmail.com>
2018-07-25 17:31:58 +0200

    Iteration 4 | Movie Page

--- starter-code/src/app/mymovie/mymovie.component.ts
+++ starter-code/src/app/mymovie/mymovie.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { Movie, MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-mymovie',
   templateUrl: './mymovie.component.html',
-  styleUrls: ['./mymovie.component.css']
+  styleUrls: ['./mymovie.component.css'],
+  providers: [ MoviesService ]
 })
 export class MymovieComponent implements OnInit {
 
-  constructor() { }
+  movie: Movie;
+
+  constructor(private moviesServ: MoviesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params) => this.movie = this.moviesServ.getMovie(Number(params["id"])));
   }
 
 }
